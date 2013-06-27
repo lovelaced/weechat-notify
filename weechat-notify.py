ddd80cd46a0213f8ffd503f611ae95a92db10768
Josh Kearney <josh@jk0.org>
2013-06-27 11:07:35 -0500

    Added email address to header.

--- weechat-notify.py
+++ weechat-notify.py
@@ -1,4 +1,4 @@
-#    Copyright 2013 Josh Kearney
+#    Copyright 2013 Josh Kearney <josh@jk0.org>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
